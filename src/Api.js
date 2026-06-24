bbadf9f8df773c55f5a7a9506fe9ebf39fb6dbc9
devanshsaxena-sys <dhruvsaxena137@gmail.com>
2026-06-24 13:54:50 +0530

    updated api

--- src/Api.js
+++ src/Api.js
@@ -1,3 +1,3 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
+const API_URL = import.meta.env.VITE_API_URL || blinkit-backend-production-d8d9.up.railway.app;
 
 export default API_URL;
