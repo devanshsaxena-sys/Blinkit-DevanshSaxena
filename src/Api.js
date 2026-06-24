b696199ea4098be9b25efc7c44440f9d77fd1ab0
devanshsaxena-sys <dhruvsaxena137@gmail.com>
2026-06-24 13:59:14 +0530

    final push guyz

--- src/Api.js
+++ src/Api.js
@@ -1,3 +1,5 @@
-const API_URL = import.meta.env.VITE_API_URL || blinkit-backend-production-d8d9.up.railway.app;
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://blinkit-backend-production-d8d9.up.railway.app/api";
 
 export default API_URL;
